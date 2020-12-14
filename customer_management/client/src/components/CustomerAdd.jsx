a3c962b494e76befbe49d1ee3d209285919e7f33
gparkkii <jyp933@gmail.com>
2020-12-14 18:25:39 +0900

    Create Customer Management Server

---
+++ customer_management/client/src/components/CustomerAdd.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { post } from 'axios';
+
+class CustomerAdd extends React.Component {
+    state = {
+        file: null,
+        userName: '',
+        age: '',
+        birthday: '',
+        job: '',
+        filename: '', 
+    }
+
+    addCustomer = () => {
+        const url = '/api/customers',
+              formData = new FormData();
+        
+        formData.append('image', this.state.file);
+        formData.append('name', this.state.userName);
+        formData.append('age', this.state.age);
+        formData.append('birthday', this.state.birthday);
+        formData.append('job', this.state.job);
+        
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+                //서버에 전달하고자 하는 내용에 파일이 있을시 웹 표준에 맞는 헤더를 추가해줘야한다.
+            }
+        }
+        return post(url, formData, config);
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        this.addCustomer()
+            .then((response) => {
+                console.log(response.data);
+            })
+    }
+
+    handleFileChange = (e) => {
+        this.setState({
+            file: e.target.files[0], //files에서 1개만 전송할 수 있게
+            fileName: e.target.value
+        })
+    }
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.userName] = e.target.value;
+        this.setState(nextState);
+    }
+
+
+    render() {
+        return(
+            <form onSubmit= {this.handleFormSubmit}>
+                <h2>고객추가</h2>
+                <div>프로필 이미지: 
+                    <input 
+                        type="file" 
+                        name="file" 
+                        file = {this.state.file} 
+                        value= {this.state.fileName} 
+                        onChange= {this.handleFileChange} 
+                    />
+                </div>
+                <div>이름: 
+                    <input 
+                        type="text" 
+                        name="userName" 
+                        value= {this.state.userName} 
+                        onChange= {this.handleValueChange} 
+                    />
+                </div>
+                <div>나이: 
+                    <input 
+                        type="number" 
+                        name="age" 
+                        value= {this.state.age} 
+                        onChange= {this.handleValueChange} 
+                    />
+                </div>
+                <div>생년월일: 
+                    <input 
+                        type="number" 
+                        name="birthday" 
+                        value= {this.state.birthday} 
+                        onChange= {this.handleValueChange} 
+                    />
+                </div>
+                <div>직업: 
+                    <input 
+                        type="text" 
+                        name="job" 
+                        value= {this.state.job} 
+                        onChange= {this.handleValueChange} 
+                    />
+                </div>
+                <button type="submit">추가하기</button>
+            </form>
+        )
+    }
+}
+
+export default CustomerAdd;
--- customer_management/server.js
+++ customer_management/server.js
@@ -1,38 +1,36 @@
+const { connect } = require("http2");
+
 const express = require("express"),
       bodyParser = require("body-parser"),
+      fs = require("fs"),
       app = express(),
       port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync("./database.json"),
+      parsedData = JSON.parse(data),
+      mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: parsedData.host,
+    user: parsedData.user,
+    password: parsedData.password,
+    port: parsedData.port,
+    database: parsedData.database
+})
+
 app.get('/api/customers', (req,res) => {
-    res.send([
-        {
-            'id': '1',
-            'image': 'https://placeimg.com/120/120/1',
-            'name': '박지연',
-            'age': '28세',
-            'birthday': '1993년 12월 1일',
-            'job': '취준생',
-        },
-        {
-            'id': '2',
-            'image': 'https://placeimg.com/120/120/2',
-            'name': '백근우',
-            'age': '26세',
-            'birthday': '1995년 10월 17일',
-            'job': '취준생',
-        },
-        {
-            'id': '3',
-            'image': 'https://placeimg.com/120/120/3',
-            'name': '보노보노',
-            'age': '알려진바 없음',
-            'birthday': '알려진바 없음',
-            'job': '취준생',
-        },
-    ]);
+   connection.query(
+       "SELECT * FROM CUSTOMER",
+       (err, rows, fields) => {
+           res.send(rows);
+       }
+   )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
+
+
+
