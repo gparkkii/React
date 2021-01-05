be1007330eee0c43d13e502f9454e021f33102d2
gparkkii <jyp933@gmail.com>
2021-01-05 14:32:16 +0900

    Create BoilerPlate Code

--- Inflearn/Boilerplate/index.js
+++ Inflearn/Boilerplate/index.js
@@ -4,10 +4,7 @@ const port = 5000;
 const { User } = require('./models/User');
 
 const bodyParser = require('body-parser');
-// bodyParser가 Client에서 오는 정보들을 서버에서 분석해서 가져올 수 있도록 해주는 코드이다.
-// 'application/x-www-form-urlencoded'처럼 된 데이터를 분석해서 가져올 수 있게 해주는 것
 app.use(bodyParser.urlencoded({extended: true}));
-// 'application/json'처럼 된 데이터를 분석해서 가져올 수 있게 해주는 것
 app.use(bodyParser.json());
 
 
