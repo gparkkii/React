b421025cb397f336328bdae9ee5d26708f4cce83
gparkkii <jyp933@gmail.com>
2021-03-04 19:53:39 +0900

    -

--- games/src/components/numberbaseball/numberbaseball.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function numberbaseball() {
-  return <div>numberbaseball</div>;
-}
-
-export default numberbaseball;
--- games/src/pages/landingpage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function landingPage() {
-  return <div>landingPage</div>;
-}
-
-export default landingPage;
